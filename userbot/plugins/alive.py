482496f5eea065be4aefde86b84fbb26eb69c9c8
shadowninja024 <63149314+shadowninja024@users.noreply.github.com>
2020-04-18 14:49:28 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -13,9 +13,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Jinda Hu Sarr ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"
+    await alive.edit("`I'm Fucking alive Moron ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"
                      f"`My peru owner`: {DEFAULTUSER}\n\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @pureindialover\n"
+                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @Shadow_ninja_legit\n"
                      "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
-                     "[Deploy this userbot Now](https://github.com/indianbhaiya/IndianBot)\n\n"
-                     "[Bot Creator](http://t.me/pureindialover)")
+                     "[Deploy this userbot Now](https://github.com/shadowninja024/Shadowninja_userbot)\n\n"
+                     "[Bot Creator](http://t.me/Shadow_ninja_legit)")
